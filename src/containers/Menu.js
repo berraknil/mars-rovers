edea6ee4769b31cfb23f4f8ed85e88a97f850514
Berrak Nil <berraknil@gmail.com>
2017-03-17 19:49:24 +0300

    onclick state update working

--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 class Menu extends Component {
-  render() {
+  constructor(props){
+    super(props);
+    this.state = { rover: ''};
+    this.selectRover = this.selectRover.bind(this);
+    
+  }
+  selectRover(name) {
+    const newState = this.state;
+    newState.rover = name;
+    this.setState({ rover: newState.rover })
+    console.log(this.state);
+  }
+  render() {    
     return (
       <div role="group" aria-label="menu">
-        <button type="button" onClick={() => console.log('curiosity')}>Curiosity</button>
-        <button type="button" onClick={() => console.log('opportunity')}>Opportunity</button>
-        <button type="button" onClick={() => console.log('spirit')}>Spirit</button>
+        <button type="button" onClick={() => this.selectRover('curiosity')}>Curiosity</button>
+        <button type="button" onClick={() => this.selectRover('opportunity')}>Opportunity</button>
+        <button type="button" onClick={() => this.selectRover('spirit')}>Spirit</button>
       </div>
     );
   }
