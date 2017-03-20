b83af16ffa79306e024a5740d03e49876f260e79
Berrak Nil <berraknil@gmail.com>
2017-03-20 14:25:42 +0300

    first draft as table complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,7 +9,7 @@ const manifest = 'manifests';
 export const FETCH_MANIFEST = 'FETCH_MANIFEST';
 
 export function fetchManifest(roverName) {
-  const url = `${ROOT_URL}${manifest}/${roverName}?api_key=q3SD3P3QY5c5yYSqH7cJE0MyUmOG0FxvQHsw2JbQ`;
+  const url = `${ROOT_URL}${manifest}/${roverName}?api_key=DEMO_KEY`;
   //'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&camera=navcam&api_key=DEMO_KEY'
   
   //const secondUrl = `${ROOT_URL}rovers/${roverName}/photos?sol=1000&page=1&camera=navcam&api_key=${KEY}`;
--- src/containers/manifest_UI.js
+++ src/containers/manifest_UI.js
@@ -2,24 +2,55 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class ManifestUI extends Component {
-  render() {
+  renderUI(data) {
     return (
+      <tr key={data.name}>
+        <td>{data.name}</td>
+        
+        <td>{data.status}</td>
+        
+        <td>{data.launch_date}</td>
+        
+        <td>{data.landing_date}</td>
+        <td>{data.max_date}</td>
+        <td>{data.max_sol}</td>
+        <td>{data.total_photos}</td>       
+      </tr>
+    ) /*
       <div>
-        <h2>Name: {console.log(this.props.manifest)} </h2>
+        <h2>Name: {console.log(data.photo_manifest)} </h2>
         <h3>Status: </h3>
         <p>Launch Date: </p>
         <p>Landing Date: </p>
         <p>Max Date: </p>
         <p>Max Sol: </p>
         <p>Total Photos: </p>
-      </div>
+        </div>*/;
+  }
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Status</th>
+            <th>Launch Date</th>
+            <th>Landing Date</th>
+            <th>Max Date</th>
+            <th>Max Sol</th>
+            <th>Total Photos</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.values(this.props.manifest).map(this.renderUI)}
+        </tbody>
+      </table>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return { manifest: state.manifest.data };
-}
-
+};
 
 export default connect(mapStateToProps)(ManifestUI);
